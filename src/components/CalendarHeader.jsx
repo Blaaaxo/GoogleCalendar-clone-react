68d516f78a498df0c3fb58c9fad9f1de570be5f8
Blaaaxo <fernandezb@tecnocomfenalco.edu.co>
2024-07-25 20:43:38 -0500

    navbar

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -1,7 +1,25 @@
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 import React from 'react'
 
 export default function CalendarHeader() {
     return (
-        <div>CalendarHeader</div>
+        <header className='px-4 py-2 flex items-center'>
+            <img className='mr-2 w-12 h-12' 
+                src="/Google_Calendar_icon.svg" alt="icon" />
+            <h1 className='mr-10 text-xl text-gray-500 font-bold'>Calendar</h1>
+            <button className='border rounded py-2 px-4 mr-5'>
+                Today
+            </button>
+            <button className='cursor-pointer text-gray-600 mx-2'>
+                <span>
+                    <ChevronLeft/>
+                </span>
+            </button>
+            <button className='cursor-pointer text-gray-600 mx-2'>
+                <span>
+                    <ChevronRight />
+                </span>
+            </button>
+        </header>
     )
 }
