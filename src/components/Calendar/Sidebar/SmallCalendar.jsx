18db504c1eeb314855656cb39166eead6e48126d
Blaaaxo <fernandezb@tecnocomfenalco.edu.co>
2024-07-26 23:39:08 -0500

    comentarios en el codigo

--- src/components/Calendar/Sidebar/SmallCalendar.jsx
+++ src/components/Calendar/Sidebar/SmallCalendar.jsx
@@ -6,27 +6,34 @@ import GlobalContext from '../../../context/GlobalContext'
 
 export default function SmallCalendar() {
 
+    // Estado para el índice del mes actual y el mes actual
     const [currentMonthIndex, setCurrentMonthIndex] = useState(dayjs().month())
     const [currentMonth, setCurrentMonth] = useState(getMonth())
 
+    // Actualiza el mes actual cuando cambia el índice del mes
     useEffect(() => {
         setCurrentMonth(getMonth(currentMonthIndex))
     }, [currentMonthIndex])
 
+    // Obtiene el índice del mes actual, el mes actual, la función para establecer el mes del calendario pequeño, el día seleccionado y el día seleccionado del contexto global
     const { monthIndex, setSmallCalendarMonth, setDaySelected, daySelected } = useContext(GlobalContext)
 
+    // Actualiza el índice del mes actual cuando cambia el índice del mes
     useEffect(() => {
         setCurrentMonthIndex(monthIndex)
     }, [monthIndex])
 
+    // Función para manejar el mes anterior
     function handlePrevMonth() {
         setCurrentMonthIndex(currentMonthIndex - 1)
     }
 
+    // Función para manejar el mes siguiente
     function handleNextMonth() {
         setCurrentMonthIndex(currentMonthIndex + 1)
     }
 
+    // Función para obtener la clase(CSS) del día actual y el día seleccionado
     function getDayClass(day) {
         const format = "DD-MM-YY"
         const today = dayjs().format(format)
@@ -60,11 +67,15 @@ export default function SmallCalendar() {
                 </div>
             </header>
             <div className='grid grid-cols-7 grid-rows-6 gap-y-1'>
+
+                {/* Renderiza los nombres de los días de la semana */}
                 {currentMonth[0].map((day, index) => (
                     <span key={index} className='text-sm py-1 text-center'>
                         {day.format('dd').charAt(0).toUpperCase()}
                     </span>
                 ))}
+                
+                {/* Renderiza los días del mes */}
                 {currentMonth.map((week, index) => (
                     <React.Fragment key={index}>
                         {week.map((day, i) => (
